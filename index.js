bc3ef9ae3b36aab35317d7db94dee182fd1e983a
AshisSardar <ashis.di.sardar@gmail.com>
2023-06-24 14:52:18 +0530

    responsiveSocialMedia

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const menuItems = document.querySelectorAll('.menu-item');
 // MESSAGES
 const messagesNotification = document.querySelector('#messages-notification');
 const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.message');
+const messageSearch = document.querySelector('#message-search');
+
+
 
 //========================SIDEBAR========================//
+
 //remove active class from all menu items
 const changeActiveItem = () => {
     menuItems.forEach(item => {
@@ -26,3 +31,34 @@ menuItems.forEach(item => {
 })
 
 //=============================MESSAGES===============================//
+//searches chats
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    message.forEach(chat => {
+        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+        if(name.indexOf(val) != -1){
+            chat.style.display = 'flex' ;
+        }else{
+            chat.style.display = 'none';
+        }
+
+    })
+}
+
+
+
+//search chat
+messageSearch.addEventListener('keyup', searchMessage);
+
+//highlight messages card when messages menu item is clicked
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() => {
+        messages.style.boxShadow = 'none';
+    }, 2000);
+})
+
+
+
+// THEME/DISPLAY CUSTOMIZATION
