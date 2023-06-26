6eaa89ba786c68ee0f52b6f794b15bfa97d1199b
AshisSardar <ashis.di.sardar@gmail.com>
2023-06-26 22:54:31 +0530

    responsiveSocialMedia

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // SIDEBAR
-const menuItems = document.querySelectorAll('.menu-item');
+const menuItems = document.querySelectorAll(' .menu-item ');
 
 // MESSAGES
 const messagesNotification = document.querySelector('#messages-notification');
@@ -11,7 +11,7 @@ const messageSearch = document.querySelector('#message-search');
 const theme = document.querySelector('#theme');
 const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
-var root = document.querySelector('root');
+var root = document.querySelector(':root');
 const colorPalette = document.querySelectorAll('.choose-color span');
 const Bg1 = document.querySelector('.bg-1');
 const Bg2 = document.querySelector('.bg-2');
@@ -34,7 +34,7 @@ menuItems.forEach(item => {
             document.querySelector('.notifications-popup').style.display='none';
         } else{
             document.querySelector('.notifications-popup').style.display='block';
-            document.querySelector('#notifications.notification-count').style.display= 'none';
+            document.querySelector('#notifications .notification-count').style.display= 'none';
         }
     })
 })
@@ -43,12 +43,12 @@ menuItems.forEach(item => {
 //searches chats
 const searchMessage = () => {
     const val = messageSearch.value.toLowerCase();
-    message.forEach(chat => {
-        let name = chat.querySelectorAll('h5').textContent.toLowerCase();
+    message.forEach(user => {
+        let name = user.querySelector('h5').textContent.toLowerCase();
         if(name.indexOf(val) != -1){
-            chat.style.display = 'flex' ;
+            user.style.display = 'flex' ;
         }else{
-            chat.style.display = 'none';
+            user.style.display = 'none';
         }
 
     })
@@ -73,21 +73,21 @@ messagesNotification.addEventListener('click', () => {
 // THEME/DISPLAY CUSTOMIZATION
 
 //opens modal
-const openThemeModel = () => {
+const openThemeModal = () => {
     themeModal.style.display = 'grid';
 }
 
 //close modal
-const closeThemeModel = (e) => {
+const closeThemeModal = (e) => {
     if(e.target.classList.contains('customize-theme')){
         themeModal.style.display = 'none';
     }
 }
 
 //close modal
-themeModal.addEventListener('click', closeThemeModel);
+themeModal.addEventListener('click', closeThemeModal);
 
-theme.addEventListener('click',openThemeModel);
+theme.addEventListener('click',openThemeModal);
 
 
 
@@ -110,24 +110,24 @@ fontSizes.forEach(size => {
     
         if(size.classList.contains('font-size-1')){
             fontSize = '10 px';
-            root.style.setProperty('----sticky-top-left' , '5.4rem');
-            root.style.setProperty('----sticky-top-right' , '5.4rem');
+            root.style.setProperty('--sticky-top-left' , '5.4rem');
+            root.style.setProperty('--sticky-top-right' , '5.4rem');
         }else if(size.classList.contains('font-size-2')){
             fontSize = '13 px';
-            root.style.setProperty('----sticky-top-left' , '5.4rem');
-            root.style.setProperty('----sticky-top-right' , '-7rem');
+            root.style.setProperty('--sticky-top-left' , '5.4rem');
+            root.style.setProperty('--sticky-top-right' , '-7rem');
         } else if(size.classList.contains('font-size-3')){
             fontSize = '16 px';
-            root.style.setProperty('----sticky-top-left' , '-2rem');
-            root.style.setProperty('----sticky-top-right' , '-17rem');
+            root.style.setProperty('--sticky-top-left' , '-2rem');
+            root.style.setProperty('--sticky-top-right' , '-17rem');
         } else if(size.classList.contains('font-size-4')){
             fontSize = '19 px';
-            root.style.setProperty('----sticky-top-left' , '-5rem');
-            root.style.setProperty('----sticky-top-right' , '-25rem');
+            root.style.setProperty('--sticky-top-left' , '-5rem');
+            root.style.setProperty('--sticky-top-right' , '-25rem');
         }else if(size.classList.contains('font-size-5')){
             fontSize = '22 px';
-            root.style.setProperty('----sticky-top-left' , '-12rem');
-            root.style.setProperty('----sticky-top-right' , '-35rem');
+            root.style.setProperty('--sticky-top-left' , '-12rem');
+            root.style.setProperty('--sticky-top-right' , '-35rem');
         }
 
     //change font size of the root html element
